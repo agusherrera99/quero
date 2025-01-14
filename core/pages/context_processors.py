b544c466425b16608687116f59a8bb9708e3af4a
agusherrera99 <agustinherrera.dev@gmail.com>
2025-01-14 14:45:57 -0300

    Feature: Add Notification model, context processor for unread notifications, and update UI to display notification count

---
+++ core/pages/context_processors.py
@@ -0,0 +1,11 @@
+# context_processors.py
+from .models import Notification
+
+def notification_count(request):
+    if request.user.is_authenticated:
+        notification_count = Notification.objects.filter(user=request.user, is_read=False).count()
+    else:
+        notification_count = 0
+    return {
+        'notification_count': notification_count
+    }
--- core/pages/models.py
+++ core/pages/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from accounts.models import CustomUser
+
 
 class BusinessType(models.Model):
     name = models.CharField(max_length=100, null=False, default='sin tipo')
@@ -18,3 +20,12 @@ class BusinessType(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Notification(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="notifications")
+    message = models.CharField(max_length=255)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
