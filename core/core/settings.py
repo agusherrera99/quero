a02b4acde221b15e0b366fa0df34a3d9bd6c9838
agusherrera99 <agustinherrera.dev@gmail.com>
2025-01-14 13:00:04 -0300

    Feature: update secret key management; replace hardcoded SECRET_KEY with environment variable for improved security

--- core/core/settings.py
+++ core/core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-30i%ec!*9igx5iaz-tzv__3cm67vk+#l3gkiv_87lacax(2ezu'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
