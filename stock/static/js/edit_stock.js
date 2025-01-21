3d8990013ef9177e328c7e1e0eba526042fd6897
agusherrera99 <agustinherrera.dev@gmail.com>
2025-01-21 09:37:20 -0300

    Refactor structure app to better deploy management

---
+++ stock/static/js/edit_stock.js
@@ -0,0 +1,22 @@
+document.getElementById('id_category').addEventListener('change', function () {
+    var categoryId = this.value;
+    if (categoryId) {
+        fetch(`/stock/load-subcategories/?category_id=${categoryId}`)
+        .then(response => response.json())
+        .then(data => {
+            var subcategoryField = document.getElementById('id_subcategory');
+            subcategoryField.innerHTML = '';
+            var option = document.createElement('option');
+            option.value = '';
+            option.text = 'Seleccione una subcategoría';
+            subcategoryField.appendChild(option);
+
+            data.subcategories.forEach(function (subcategory) {
+                var option = document.createElement('option');
+                option.value = subcategory.id;
+                option.text = subcategory.name;
+                subcategoryField.appendChild(option);
+            });
+        })
+    }
+});
