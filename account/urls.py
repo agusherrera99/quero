ebad685a9709aadb4b6ffa25cdb236a44bab0284
agusherrera99 <agustinherrera.dev@gmail.com>
2025-01-29 17:08:53 -0300

    Temprary refactor: Remove update plan and plans-prices links from templates and URLs

--- account/urls.py
+++ account/urls.py
@@ -11,6 +11,5 @@ urlpatterns = [
     path('register/', views.registration_view, name='register'),
     path('select-business-type/', views.business_type_selection, name='business_type_selection'),
     path('select-business-type/submit/', views.select_business_type, name='select_business_type'),
-    path('update-plan/', views.update_plan, name='update_plan'),
     path('mark-as-read/<int:notification_id>/', views.mark_notification_as_read, name='mark_notification_as_read'),
 ]
--- pages/urls.py
+++ pages/urls.py
@@ -7,7 +7,6 @@ app_name = 'pages'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('plans-prices/', views.prices, name='plans-prices'),
     path('contact/', views.contact, name='contact'),
     path('contact/submit/', views.receive_contact_email, name='receive_contact_email'),
 ]
