6dac17fd3d8464e388079b3b460ecd106ddab55e
agusherrera99 <agustinherrera.dev@gmail.com>
2025-04-06 09:48:56 -0300

    Update: admin models for Category, Subcategory, and Product with enhanced display and search options

--- stock/admin.py
+++ stock/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, Subcategory, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+    list_filter = ('name',)
+    ordering = ('name',)
+    list_per_page = 10
+    list_editable = ('name',)
+    fieldsets = (
+        (None, {
+            'fields': ('name',)
+        }),
+    )
+    readonly_fields = ('id',)
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category')
+    search_fields = ('name',)
+    list_filter = ('category',)
+    ordering = ('name',)
+    list_per_page = 10
+    list_editable = ('name', 'category')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'category')
+        }),
+    )
+    readonly_fields = ('id',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'quantity', 'price', 'cost', 'barcode', 'uom', 'subcategory')
+    search_fields = ('name', 'barcode')
+    list_filter = ('subcategory',)
+    ordering = ('name',)
+    list_per_page = 10
+    list_editable = ('name', 'quantity', 'price', 'cost', 'barcode', 'uom', 'subcategory')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'quantity', 'price', 'cost', 'barcode', 'uom', 'subcategory')
+        }),
+    )
+    readonly_fields = ('id',)
