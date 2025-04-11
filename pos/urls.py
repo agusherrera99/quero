d83f3e00ce6123d7804d1c2d123a2554b7ff2ada
agusherrera99 <agustinherrera.dev@gmail.com>
2025-04-11 11:47:02 -0300

    Update: remove scan functionality from templates and URLs (temporary)

--- pos/urls.py
+++ pos/urls.py
@@ -7,8 +7,8 @@ app_name = 'pos'
 
 urlpatterns = [
     path('', views.pos, name='pos'),
-    path('scan/', views.scan_product, name='scan_product'),
-    path('scan/search/', views.search_scan_product, name='search_scan_product'),
+    # path('scan/', views.scan_product, name='scan_product'),
+    # path('scan/search/', views.search_scan_product, name='search_scan_product'),
     path('confirm/', views.sales_confirmation, name='sales_confirmation'),
     path('confirm/process/', views.process_sale, name='process_sale'),
     path('cancel/', views.cancel_sale, name='cancel_sale'),
--- stock/urls.py
+++ stock/urls.py
@@ -13,9 +13,9 @@ urlpatterns = [
     path('load-subcategories/', views.load_subcategories, name='load_subcategories'),
     path('add/', views.add_stock, name='add_stock'),
     path('edit/<int:pk>/', views.edit_stock, name='edit_stock'),
-    path('scan/', views.scan_stock, name='scan_stock'),
-    path('scan/get_category_name/', views.get_category_name, name='get_category_name'),
-    path('scan/get_subcategory_name/', views.get_subcategory_name, name='get_subcategory_name'),
-    path('scan/add', views.add_scan, name='add_scan'),
+    # path('scan/', views.scan_stock, name='scan_stock'),
+    # path('scan/get_category_name/', views.get_category_name, name='get_category_name'),
+    # path('scan/get_subcategory_name/', views.get_subcategory_name, name='get_subcategory_name'),
+    # path('scan/add', views.add_scan, name='add_scan'),
     path('delete/<int:pk>/', views.delete_stock, name='delete_stock'),
 ]
